41afefc5d427b4ae4b822054b2e47f809e4bc03c
Andrew0502 <agcmarcus@gmail.com>
2020-10-05 21:31:37 -0400

    Added role table and columns.

--- schema.sql
+++ schema.sql
@@ -7,5 +7,16 @@ USE trackerDB;
 CREATE TABLE employee (
   id INTEGER NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30),
-  PRIMARY KEY ()
+  last_name VARCHAR(30),
+  role_id INTEGER, 
+  manager_id INTEGER,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary decimal, 
+    department_id INTEGER,
+    PRIMARY KEY (id)
 );
