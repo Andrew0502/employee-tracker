a5e90a32ac3b6e74fe7733120ca7180a6f2c0baa
Andrew0502 <agcmarcus@gmail.com>
2020-10-05 23:39:58 -0400

    Added connection to mysql into index.js.

---
+++ index.js
@@ -0,0 +1,39 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Andrew0502',
+  database: 'trackerDB'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+  });
+
+  function init(){
+
+  }
+
+//   function start() {
+//     inquirer
+//       .prompt({
+//         name: 'postOrBid',
+//         type: 'list',
+//         message: 'Would you like to [POST] an auction or [BID] on an auction?',
+//         choices: ['POST', 'BID', 'EXIT']
+//       })
+//       .then(function (answer) {
+//         // based on their answer, either call the bid or the post functions
+//         if (answer.postOrBid === 'POST') {
+//           postAuction();
+//         } else if (answer.postOrBid === 'BID') {
+//           bidAuction();
+//         } else {
+//           connection.end();
+//         }
+//       });
+//   }
