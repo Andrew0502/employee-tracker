c9cf70c7b2ce1f349295d26c9cf69b5d0f3e6737
Andrew0502 <agcmarcus@gmail.com>
2020-10-06 20:38:39 -0400

    Built out view and update functions. Added addRole and addNewRole functions. Confirmed functionality of all.

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ connection.connect(function (err) {
     init();
   });
 
+//===========================================================================
 
   function init() {
     inquirer
       .prompt({
         name: 'whatDo',
         type: 'list',
-        message: 'Would you like to do?',
-        choices: ['ADD', 'VIEW','UPDATE', 'EXIT']
+        message: 'What would you like to do?',
+        choices: ['ADD','VIEW','UPDATE','EXIT']
       })
       .then(function (answer) {
         if (answer.whatDo === 'ADD') {
@@ -34,8 +35,10 @@ connection.connect(function (err) {
         } else if (answer.whatDo === 'EXIT') {
           connection.end();
         }
-      });
+    });
   }
+
+//===========================================================================
 
   function add() {
     inquirer
@@ -49,26 +52,92 @@ connection.connect(function (err) {
         if (answer.whatAdd === 'Department') {
           addDepartment();
         } else if (answer.whatAdd === 'Role') {
-          AddRole();
+          addRole();
         } else if (answer.whatAdd === 'Employee') {
-          AddEmployee();
+          addEmployee();
         } else if (answer.whatAdd === 'I changed my mind.') {
           init();
         }
-      });
+    });
   }
+
+  function view() {
+    inquirer.prompt({
+      name: 'whatView',
+      type: 'list',
+      message: 'What would you like to view?',
+      choices: ['Department', 'Role', 'Employee', 'I changed my mind.']
+    }).then((viewAnswer) => {
+        if(viewAnswer.whatView === 'Department') {
+          viewDepartment();
+        } else if (viewAnswer.whatView === 'Role') {
+          viewRole();
+        } else if (viewAnswer.whatView === 'Employee') {
+          viewEmployee();
+        } else if (viewAnswer.whatView === 'I changed my mind.') {
+          init();
+        }
+    });
+  }
+
+  function update() {
+    inquirer
+    .prompt({
+      name: 'whatUpdate',
+      type: 'list',
+      message: 'What would you like to update?',
+      choices: ['Department', 'Role','Employee', 'I changed my mind.']
+    })
+    .then(function (answer) {
+      if (answer.whatUpdate === 'Department') {
+        updateDepartment();
+      } else if (answer.whatUpdate === 'Role') {
+        updateRole();
+      } else if (answer.whatUpdate === 'Employee') {
+        updateEmployee();
+      } else if (answer.whatUpdate === 'I changed my mind.') {
+        init();
+      }
+    });
+  }
+
+//===========================================================================
 
   function addDepartment() {
     inquirer
       .prompt({
         name: 'depName',
         type: 'input',
-        message: 'What do you want to name the new Department?',
+        message: 'What do you want to name the new department?',
       }).then(({depName}) =>{
-        console.log(depName);
+        // console.log(depName)
         addNewDep(depName);
       })
   }
+
+  function addRole() {
+    inquirer
+      .prompt([{
+        name: 'roleName',
+        type: 'input',
+        message: 'What title do you want the new role to have?',
+      },
+      {
+          name: 'roleSalary',
+          type: 'input',
+          message: 'What salary do you want the new role to have?',
+      },
+      {
+            name: 'roleDepId',
+            type: 'input',
+            message: 'What department id do you want the new role to have?'
+      }]).then(({roleName, roleSalary, roleDepId}) =>{
+        console.log(roleName, roleSalary, roleDepId);
+        addNewRole(roleName, roleSalary, roleDepId);
+      })
+  }
+
+//===========================================================================
 
   function addNewDep(name){
     connection.query("INSERT INTO department SET ? ", 
@@ -79,10 +148,12 @@ connection.connect(function (err) {
     });
   }
 
-  function view() {
-
-  }
-
-  function update() {
-
+  function addNewRole(title, salary, department_id){
+    connection.query("INSERT INTO role SET ? ", 
+    {title: title, salary: salary, department_id: department_id}, 
+    (error, data) => {
+        if (error) throw error;
+        console.table(data);
+        init();
+    });
   }
